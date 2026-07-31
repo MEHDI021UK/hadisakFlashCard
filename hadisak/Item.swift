6660f2aa96a8dc1816ab40cc411e32afe9ebd889
MEHDI021UK <mehdisehati.uk@gmail.com>
2026-07-31 22:47:43 +0100

    Initial Commit

---
+++ hadisak/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  hadisak
+//
+//  Created by user on 2026-07-31.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ hadisakTests/hadisakTests.swift
@@ -0,0 +1,17 @@
+//
+//  hadisakTests.swift
+//  hadisakTests
+//
+//  Created by user on 2026-07-31.
+//
+
+import Testing
+@testable import hadisak
+
+struct hadisakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
